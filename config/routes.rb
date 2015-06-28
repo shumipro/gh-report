be102a7a6c5d2c7b8fc219d1abca509328afa193
Nao Minami <minami@wantedly.com>
2015-06-28 09:18:07 +0900

    Add root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 # == Route Map
 #
 # Prefix Verb URI Pattern                        Controller#Action
+#   root GET  /                                  roots#index
 #        GET  /auth/:provider/callback(.:format) sessions#create
 #
 
 Rails.application.routes.draw do
+  root to: 'roots#index'
   get '/auth/:provider/callback', to: 'sessions#create'
 end
 
