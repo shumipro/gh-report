b9ba32ed7292a5295d882fba5eb112fb0d3dff84
Nao Minami <minami@wantedly.com>
2015-07-12 11:29:37 +0900

    Add GithubService.today_events

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,6 @@
 class ReportsController < ApplicationController
   def index
-    reports = Report.new(GithubService.get_events(current_user)).markdown
+    reports = Report.new(GithubService.today_events(current_user)).markdown
     @markdown = MarkdownRenderer.render(reports)
   end
 end
--- app/models/github_service.rb
+++ app/models/github_service.rb
@@ -1,6 +1,11 @@
 class GithubService
-  def self.get_events(user)
+  def self.events(user, range:)
     client = Octokit::Client.new(login: user.nickname, access_token: user.token)
-    client.user_events(user.nickname, page: 1)
+    events = client.user_events(user.nickname, page: 1)
+    events.select { |e| range.first < e[:created_at] && e[:created_at] < range.last }
+  end
+
+  def self.today_events(user)
+    self.events(user, range: Date.today.beginning_of_day..Date.today.end_of_day)
   end
 end
