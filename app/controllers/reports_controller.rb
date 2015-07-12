4cefb8646dadf8410e8f84819259b6f4a27a9501
Nao Minami <minami@wantedly.com>
2015-07-12 11:49:49 +0900

    Add GithubService.weekly_events

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,12 @@
 class ReportsController < ApplicationController
   def index
-    reports = Report.new(GithubService.today_events(current_user)).markdown
+    events = if params[:weekly].present?
+      events = GithubService.weekly_events(current_user)
+    else
+      events = GithubService.daily_events(current_user)
+    end
+
+    reports = Report.new(events).markdown
     @markdown = MarkdownRenderer.render(reports)
   end
 end
--- app/models/github_service.rb
+++ app/models/github_service.rb
@@ -5,13 +5,17 @@ class GithubService
     selected_events = events.select { |e| range.first < e[:created_at] && e[:created_at] < range.last }
 
     if selected_events.size == events.size
-      selected_events + self.events(user, range: range, page: page + 1)
+      selected_events + self.events(user, range: range, page: (page + 1))
     else
       selected_events
     end
   end
 
-  def self.today_events(user)
+  def self.daily_events(user)
     self.events(user, range: Date.today.beginning_of_day..Date.today.end_of_day)
+  end
+
+  def self.weekly_events(user)
+    self.events(user, range: 1.week.ago.beginning_of_day..Date.today.end_of_day)
   end
 end
